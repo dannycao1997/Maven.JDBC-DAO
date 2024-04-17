af46066b29a9f91efb1cc9d66aa607c3c88c4171
Danny Cao <dannycao1997@yahoo.com>
2024-04-17 14:45:27 -0400

    injected new dependencies to pom.xl file, created new cars database, created car class

---
+++ src/main/java/models/Cars.java
@@ -0,0 +1,71 @@
+package models;
+
+public class Cars {
+
+    private Integer id;
+    private String make;
+    private String model;
+    private Integer year;
+    private String color;
+    private String vin;
+
+    public Cars(){
+    }
+
+    public Cars(Integer id, String make, String model, Integer year, String color, String vin){
+        this.id = id;
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.vin = vin;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public Integer getYear(){
+        return year;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public String getVin(){
+        return vin;
+    }
+
+    public void setId(Integer id){
+        this.id = id;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setModel(String model){
+        this.model = model;
+    }
+
+    public void setYear(Integer year){
+        this.year = year;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    public void setVin(String vin){
+        this.vin = vin;
+    }
+}
